0e665b3e6da35f84070c12736f6223c593eeb690
Avaro <alvaro.caso@alumnos.uva.es>
2018-12-15 20:32:07 +0100

    Funciona, solo queda ponerlo bonito

--- app/src/main/java/com/alvaro/sergio/smov_yamba/TimelineFragment.java
+++ app/src/main/java/com/alvaro/sergio/smov_yamba/TimelineFragment.java
@@ -3,21 +3,21 @@ package com.alvaro.sergio.smov_yamba;
 import android.app.ListFragment;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-public class TimelineFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>  {
+
+public class TimelineFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>{
     private static final String TAG = TimelineFragment.class.getSimpleName();
     private SimpleCursorAdapter mAdapter;
     private static final String[] FROM = {SupportServices.USER,
-            SupportServices.MESSAGE, SupportServices.CREATED_AT};
+           SupportServices.MESSAGE, SupportServices.CREATED_AT};
     private static final int[] TO = {R.id.list_item_text_user,
             R.id.list_item_text_message, R.id.list_item_text_created_at};
 
@@ -26,19 +26,24 @@ public class TimelineFragment extends ListFragment implements LoaderManager.Load
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         setEmptyText("Sin datos...");
+
         mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item,
                 null, FROM, TO, 0);
+
+        mAdapter.setViewBinder(new TimelineViewBinder());
+
         setListAdapter(mAdapter);
+
+        setListAdapter(mAdapter);
+
         getLoaderManager().initLoader(LOADER_ID, null, this);
 
     }
 
-
-
-    @NonNull
     @Override
-    public android.content.Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         if (i != LOADER_ID)
             return null;
         Log.d(TAG,
@@ -46,15 +51,15 @@ public class TimelineFragment extends ListFragment implements LoaderManager.Load
         return new CursorLoader(getActivity(), SupportServices.CONTENT_URI, null, null,
                 null, SupportServices.DEFAULT_SORT);
     }
-
     @Override
-    public void onLoadFinished(android.content.Loader<Cursor> loader, Cursor data) {
-
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        Log.d(TAG,
+                "onLoadFinished with cursor: " + cursor.getCount());
+        mAdapter.swapCursor(cursor);
     }
-
     @Override
-    public void onLoaderReset(android.content.Loader<Cursor> loader) {
-
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
     }
 
     class TimelineViewBinder implements SimpleCursorAdapter.ViewBinder {
@@ -62,12 +67,12 @@ public class TimelineFragment extends ListFragment implements LoaderManager.Load
         public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
             if (view.getId() != R.id.list_item_text_created_at)
                 return false;
-// Convertimos el timestamp a tiempo relativo
             long timestamp = cursor.getLong(columnIndex);
             CharSequence relativeTime = DateUtils.getRelativeTimeSpanString(timestamp);
             ((TextView)view).setText(relativeTime);
             return true;
         }
     }
-
 }
+
+
